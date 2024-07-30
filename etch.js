9fbcbb56ad41e16eb90b37cb1fa8cf1c8922afd0
Tofer-Cich <[tofercich@hotmail.com]>
2024-07-30 14:41:15 -0500

    Update btn to make new grid, dynamic color/opacity
    grid button allows user to customize size to their liking. dynamic rgb
    values to make pretty colors through out canvas.

---
+++ etch.js
@@ -0,0 +1,77 @@
+const grid = document.querySelector("#container");
+const body = document.querySelector("body");
+let redRandom;
+let greenRandom;
+let blueRandom;
+
+// add button that says new grid
+// when clicked, prompt user for size
+// store user in global variable called size
+// limit range between 1 and 100 inclusive
+// use the for loops to make a new grid
+
+const gridButton = document.createElement("button");
+gridButton.classList.add("btn");
+gridButton.textContent = "New Grid";
+
+body.insertBefore(gridButton, body.firstChild);
+
+gridButton.addEventListener("click", () => {
+    let userSize = prompt("Give me a number");
+
+    if (userSize > 0 && userSize <= 100) {
+
+        Array.from(grid.children).forEach(child => {
+            child.remove();
+        });
+
+        for (let i = 0; i < userSize; i++) {
+            const row = document.createElement("div");
+            row.classList.add("row");
+
+            for (let j = 0; j < userSize; j++) {
+                const square = document.createElement("div");
+                square.classList.add("square");
+                square.style.opacity = 0;
+                row.appendChild(square);
+
+                square.addEventListener("mouseenter", () => {
+                    redRandom = Math.floor(Math.random() * 255);
+                    greenRandom = Math.floor(Math.random() * 255);
+                    blueRandom = Math.floor(Math.random() * 255);
+
+                    square.style.opacity = parseFloat(square.style.opacity) + .1;
+                    square.style.background = `rgb(${redRandom}, ${greenRandom}, ${blueRandom})`;
+                });
+            }
+
+            grid.appendChild(row);
+        }
+    } else {
+        alert("Number must be between 1 and 100");
+    }
+});
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.opacity = 0;
+        row.appendChild(square);
+
+        square.addEventListener("mouseenter", () => {
+            redRandom = Math.floor(Math.random() * 255);
+            greenRandom = Math.floor(Math.random() * 255);
+            blueRandom = Math.floor(Math.random() * 255);
+
+            square.style.opacity = parseFloat(square.style.opacity) + .1;
+            square.style.background = `rgb(${redRandom}, ${greenRandom}, ${blueRandom})`;
+        });
+    }
+
+    grid.appendChild(row);
+}
+
