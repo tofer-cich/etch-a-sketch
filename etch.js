f514ed64c6c4044c170dd770f639aada67f94e4a
Tofer-Cich <[tofercich@hotmail.com]>
2025-07-11 15:35:49 -0500

    Add pointermove and touchmove listeners

--- etch.js
+++ etch.js
@@ -65,6 +65,16 @@ for (let i = 0; i < 16; i++) {
             square.style.background = `rgb(${redRandom}, ${greenRandom}, ${blueRandom})`;
         });
 
+        square.addEventListener("touchmove", (e) => {
+            e.preventDefault();
+            redRandom = Math.floor(Math.random() * 255);
+            greenRandom = Math.floor(Math.random() * 255);
+            blueRandom = Math.floor(Math.random() * 255);
+
+            square.style.opacity = parseFloat(square.style.opacity) + .1;
+            square.style.background = `rgb(${redRandom}, ${greenRandom}, ${blueRandom})`;
+        });
+
     }
 
     grid.appendChild(row);
